e2fe8c8a0a846f2c56add2a7f1053faed3ea5a4e
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-08 21:59:36 -0300

    Update RouteList.tsx

--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,8 +1,10 @@
 import { useRoutes } from 'react-router-dom'
+import Albums from '../pages/Albums/Albums'
 import Home from '../pages/Home/Home'
 
 export const RouteList = () => {
   return useRoutes([
-    { path: '/', element: <Home /> }
+    { path: '/', element: <Home /> },
+    { path: '/albums/:id', element: <Albums /> }
   ])
 }
