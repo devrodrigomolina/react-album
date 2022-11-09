34bfbc3c307adaca46cc140e71f5282bf1b0c92c
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-11-08 21:59:34 -0300

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,32 +1,37 @@
-import React, { useEffect, useState } from 'react'
-import { api } from '../../services/api'
-
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import AlbumItens from "../../components/AlbumItens/AlbumItens";
+import { api } from "../../services/api";
 
 type Albums = {
-  id: number,
-  title: string,
-  userId: number
-}
+  id: number;
+  title: string;
+  userId: number;
+};
 
 const Home = () => {
-  const [albums, setAlbums] = useState<Albums[]>([])
+  const [albums, setAlbums] = useState<Albums[]>([]);
 
   const getAlbums = async () => {
-    const { data: json } = await api.get('/albums')
-    setAlbums(json)
-  }
+    const { data: json } = await api.get(`/albums`);
+    setAlbums(json);
+  };
 
   useEffect(() => {
-    getAlbums()
-  }, [])
+    getAlbums();
+  }, []);
 
   return (
     <ul>
-      {albums.map(({ title, id }) => (
-        <li key={id}>{title}</li>
+      {albums.map((item, index) => (
+        <AlbumItens 
+          key={index}
+          title={item.title}
+          id={item.id} 
+        />
       ))}
     </ul>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
