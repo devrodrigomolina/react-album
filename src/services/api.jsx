9846e01e4101deaa38c64aa483a035fd3b8556b5
FOXXX <75998678+devrodrigomolina@users.noreply.github.com>
2022-12-06 20:52:43 -0300

    Update api.jsx

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,11 +1,24 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({
+export const request = axios.create({
   baseURL: 'https://jsonplaceholder.typicode.com'
 })
 
 export const api = {
-  get(endpoint) {
-    return axiosInstance.get(endpoint)
+  getAlbum: async () => {
+    const req = await request('/albums')
+    return req.data
+  },
+  getAlbumId: async (id) => {
+    const req = await request(`/albums/${id}`)
+    return req.data
+  },
+  getAlbumPhotos: async (id) => {
+    const req = await request(`/albums/${id}/photos`)
+    return req.data
+  },
+  getPhoto: async (id) => {
+    const req = await request(`/photos/${id}`)
+    return req.data
   }
-}
+};
